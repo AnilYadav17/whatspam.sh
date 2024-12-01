6347b39987245fa2f6bc23b0370779ccb1bba513
Anil Yadav <153111753+AnilYadav17@users.noreply.github.com>
2024-12-01 03:42:58 +0000

    Add files via upload

---
+++ whatsapp_spam.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MESSAGE="TATTI"   # Message to be sent
+COUNT=10000        # Number of times to send the message
+
+for i in $(seq 1 $COUNT); do
+    xdotool type --delay 1 "$MESSAGE"  # Minimal delay (1 ms) for ultra-fast speed
+    xdotool key Return                 # Press Enter to send
+done
